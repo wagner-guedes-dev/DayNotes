cc2dd6975af299c689711a7537955d61d04bce4b
wagnerwg0202 <wagnerguedes123@hotmail.com>
2023-01-04 03:11:24 -0300

    backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,19 @@ const express = require('express');
 const routes = express.Router();
 
 const AnnotationController = require('./controllers/AnnotationsController');
+const PriorityController = require('./controllers/PriorityController');
+const ContentController = require('./controllers/ContentController');
 
 //Rota Annotations
 routes.post('/annotations', AnnotationController.create);
 routes.get('/annotations', AnnotationController.read);
 routes.delete('/annotations/:id', AnnotationController.delete )
 
+//Rota Priority
+routes.get('/priorities', PriorityController.read)
+routes.post('/priorities/:id', PriorityController.update)
+
+//rota content
+routes.post('/contents/:id', ContentController.update)
 
 module.exports = routes
