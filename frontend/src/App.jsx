811eb983adb2dce7bd57363cabd93c7013e7043d
wagnerwg0202 <wagnerguedes123@hotmail.com>
2023-01-05 21:20:34 -0300

    layot created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,38 @@
 import { useState } from 'react'
 
 import './App.css'
+import Notes from './components/Notes'
 
 function App() {
 
 
   return (
     <div className="App">
-      <h1>
-        DayNotes
-      </h1>
+      <aside>
+        <strong>Caderno de Notas</strong>
+        <form>
+
+          <div className='input-block'>
+            <label htmlFor='title'>Titulo da Anotação</label>
+            <input/>
+          </div>
+
+          <div className='input-block'>
+            <label htmlFor='nota'>Anotações</label>
+            <textarea></textarea>
+          </div>
+
+            <button type='submit'>Salvar</button>
+
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <Notes/>
+        </ul>
+      </main>
+
     </div>
   )
 }
